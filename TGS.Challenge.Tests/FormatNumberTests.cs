b0671d8f5d5f5b4a515c44f20c0559cabe216152
Mark <58310560+markorpwood97@users.noreply.github.com>
2021-08-12 19:21:17 +0200

    FormatNumber: Correcting unit tests

--- TGS.Challenge.Tests/FormatNumberTests.cs
+++ TGS.Challenge.Tests/FormatNumberTests.cs
@@ -36,7 +36,7 @@ namespace TGS.Challenge.Tests
     [Test()]
     public void Value_10_Returns_ValidString()
     {
-      var formatted = _formatNumber.Format(1);
+      var formatted = _formatNumber.Format(10);
 
       Assert.AreEqual("10", formatted);
     }
@@ -44,15 +44,15 @@ namespace TGS.Challenge.Tests
     [Test()]
     public void Value_100_Returns_ValidString()
     {
-      var formatted = _formatNumber.Format(1);
+      var formatted = _formatNumber.Format(100);
 
-      Assert.AreEqual("1,00", formatted);
+      Assert.AreEqual("100", formatted);
     }
 
     [Test()]
     public void Value_1000_Returns_ValidString()
     {
-      var formatted = _formatNumber.Format(1);
+      var formatted = _formatNumber.Format(1000);
 
       Assert.AreEqual("1,000", formatted);
     }
@@ -60,7 +60,7 @@ namespace TGS.Challenge.Tests
     [Test()]
     public void Value_10000_Returns_ValidString()
     {
-      var formatted = _formatNumber.Format(1);
+      var formatted = _formatNumber.Format(10000);
 
       Assert.AreEqual("10,000", formatted);
     }
@@ -68,7 +68,7 @@ namespace TGS.Challenge.Tests
     [Test()]
     public void Value_100000_Returns_ValidString()
     {
-      var formatted = _formatNumber.Format(1);
+      var formatted = _formatNumber.Format(100000);
 
       Assert.AreEqual("100,000", formatted);
     }
@@ -76,7 +76,7 @@ namespace TGS.Challenge.Tests
     [Test()]
     public void Value_1000000_Returns_ValidString()
     {
-      var formatted = _formatNumber.Format(1);
+      var formatted = _formatNumber.Format(1000000);
 
       Assert.AreEqual("1,000,000", formatted);
     }
@@ -84,9 +84,9 @@ namespace TGS.Challenge.Tests
     [Test()]
     public void Value_35235235_Returns_ValidString()
     {
-      var formatted = _formatNumber.Format(1);
+      var formatted = _formatNumber.Format(35235235);
 
-      Assert.AreEqual("35,2352,35", formatted);
+      Assert.AreEqual("35,235,235", formatted);
     }
   }  
 }
